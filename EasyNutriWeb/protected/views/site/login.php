bc4f17f8387ed64ac91bcb6ef31ac973eaaaac90
Sofia Oliveira <sofia_oliveira9@hotmail.com>
2014-12-18 19:37:05 +0000

    pagina login mudado !

--- EasyNutriWeb/protected/views/site/login.php
+++ EasyNutriWeb/protected/views/site/login.php
@@ -9,45 +9,37 @@ $this->breadcrumbs=array(
 );
 ?>
 
-<h1>Login</h1>
 
-<p>Please fill out the following form with your login credentials:</p>
 
-<div class="form">
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'login-form',
-	'enableClientValidation'=>true,
-	'clientOptions'=>array(
-		'validateOnSubmit'=>true,
-	),
-)); ?>
+<div class="form_login">
+    <?php $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
+        'layout' => TbHtml::FORM_LAYOUT_HORIZONTAL,
+        'id' => 'login-form'
+    )); ?>
+ <br>
+    <br>
+    <?php echo $form->errorSummary($model); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'username'); ?>
-		<?php echo $form->textField($model,'username'); ?>
-		<?php echo $form->error($model,'username'); ?>
+		<?php echo $form->textFieldControlGroup($model,'username'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'password'); ?>
-		<?php echo $form->passwordField($model,'password'); ?>
-		<?php echo $form->error($model,'password'); ?>
-		<p class="hint">
-			Hint: You may login with <kbd>demo</kbd>/<kbd>demo</kbd> or <kbd>admin</kbd>/<kbd>admin</kbd>.
-		</p>
+		<?php echo $form->passwordFieldControlGroup($model,'password'); ?>
 	</div>
 
 	<div class="row rememberMe">
-		<?php echo $form->checkBox($model,'rememberMe'); ?>
-		<?php echo $form->label($model,'rememberMe'); ?>
-		<?php echo $form->error($model,'rememberMe'); ?>
+		<?php echo $form->checkBoxControlGroup($model,'rememberMe'); ?>
 	</div>
 
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Login'); ?>
-	</div>
+<!--	<div class="row buttons">-->
+<!--		--><?php //echo CHtml::submitButton('Login',  array('color' => TbHtml::BUTTON_COLOR_PRIMARY)); ?>
+<!--	</div>-->
+
+    <?php echo TbHtml::formActions(array(
+        TbHtml::submitButton('Login', array('id'=>'btnLogin', 'color' => TbHtml::BUTTON_COLOR_DEFAULT )),
+    )); ?>
 
 <?php $this->endWidget(); ?>
 </div><!-- form -->
