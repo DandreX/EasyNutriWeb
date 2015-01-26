6a06ce77d4aa9fc98ef0792f6fba9a0532e3a48a
DandreX <andre_d-x@hotmail.com>
2015-01-26 19:22:13 +0000

    pagina inicial

--- EasyNutriWeb/protected/views/site/index.php
+++ EasyNutriWeb/protected/views/site/index.php
@@ -1,20 +1,21 @@
 <?php
 /* @var $this SiteController */
-$this->pageTitle=Yii::app()->name;
+$this->pageTitle = Yii::app()->name;
 ?>
 
-    <img src="<?php echo Yii::app()->getBaseUrl().'/assets/imagensEasynutri/iconeTransparency.png'?>"  id="imgEasyNutri">
+<img src="<?php echo Yii::app()->getBaseUrl() . '/assets/imagensEasynutri/iconeTransparency.png' ?>" id="imgEasyNutri">
 
 
 <div id="mobileApps">
-<a href="https://play.google.com/store/apps/details?id=com.ionicframework.easynutrimobile576589">
-    <img alt="Android app on Google Play"
-         src="<?php echo Yii::app()->getBaseUrl().'/assets/imagensEasynutri/android-app-on-google-play-logo-vector-2.png'?>" />
-</a>
+    <h3>Aceda à aplicação cliente em:</h3>
+    <a href="https://play.google.com/store/apps/details?id=com.ionicframework.easynutrimobile576589">
+        <img alt="Android app on Google Play"
+             src="<?php echo Yii::app()->getBaseUrl() . '/assets/imagensEasynutri/android-app-on-google-play-logo-vector-2.png' ?>"/>
+    </a>
     <a href="http://dei.estg.ipleiria.pt/projetos/easynutri/mobile/www/#/login">
         <img alt="Available on Web"
-             src="<?php echo Yii::app()->getBaseUrl().'/assets/imagensEasynutri/RedeAppWebAppIcon-Browsers.png'?> " />
-        </a>
+             src="<?php echo Yii::app()->getBaseUrl() . '/assets/imagensEasynutri/RedeAppWebAppIcon-Browsers.png' ?> "/>
+    </a>
 </div>
 
 
